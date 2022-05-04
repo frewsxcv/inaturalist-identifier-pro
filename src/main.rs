64aa75a687ea9181ab1a7a92b644ff6da02ca880
Corey Farwell <coreyf@rwell.org>
2022-05-04 11:40:24 -0400

    rate limiter

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ use std::{collections, error, fs, thread, time};
 const PLANTAE_ID: u32 = 47126;
 
 lazy_static::lazy_static! {
-static ref INATURALIST_REQUEST_CONFIG: inaturalist::apis::configuration::Configuration =
-    inaturalist::apis::configuration::Configuration {
-        base_path: String::from("https://api.inaturalist.org/v1"),
-        ..Default::default()
-    };
+    static ref INATURALIST_REQUEST_CONFIG: inaturalist::apis::configuration::Configuration =
+        inaturalist::apis::configuration::Configuration {
+            base_path: String::from("https://api.inaturalist.org/v1"),
+            ..Default::default()
+        };
+    static ref INATURALIST_RATE_LIMITER: governor::RateLimiter<
+        governor::state::direct::NotKeyed,
+        governor::state::InMemoryState,
+        governor::clock::DefaultClock,
+    > =
+        governor::RateLimiter::direct(
+            governor::Quota::per_second(1.try_into().unwrap()),
+        );
 }
 
 #[tokio::main]
@@ -16,6 +24,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     // Brooklyn
     // let sw = geo::coord! { x: -74.046000f64, y: 40.567 };
     // let ne = geo::coord! { x: -73.9389741f64, y: 40.6942535f64 };
+
 
     let sw = geo::coord! { x: -74.258019, y: 40.490742 };
     let ne = geo::coord! { x: -73.555615, y: 41.017433 };
@@ -26,7 +35,11 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
 
     let mut entries = vec![];
 
-    let observations = fetch(rect).await?;
+    let rects = subdivide_rect(rect).await?;
+    let mut observations = vec![];
+    for rect in rects {
+        observations.append(&mut fetch(rect).await?);
+    }
 
     for (i, rect) in grid_iter(rect, divisions).enumerate() {
         println!("Building new tile ({} / {})", i, divisions * divisions);
@@ -47,13 +60,73 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         entries.push((rect, observations_species_count(&observations_in_tile)));
     }
 
-    fs::write("/Users/coreyf/tmp/output.geojson", to_geojson(entries).to_string())?;
+    fs::write(
+        "/Users/coreyf/tmp/output.geojson",
+        to_geojson(entries).to_string(),
+    )?;
 
     Ok(())
 }
 
 type Entry = (geo::Rect<f64>, usize);
 type Entries = Vec<Entry>;
+
+/// iNaturalist will not let us page past 10,000 results.
+const MAX_RESULTS: i32 = 10_000;
+
+#[async_recursion::async_recursion]
+async fn subdivide_rect(
+    rect: geo::Rect<f64>,
+) -> Result<
+    Vec<geo::Rect<f64>>,
+    inaturalist::apis::Error<inaturalist::apis::observations_api::ObservationsGetError>,
+> {
+    INATURALIST_RATE_LIMITER.until_ready().await;
+    let response = inaturalist::apis::observations_api::observations_get(
+        &INATURALIST_REQUEST_CONFIG,
+        build_params(rect, 1, 1),
+    )
+    .await?;
+
+    Ok(if response.total_results.unwrap() < MAX_RESULTS {
+        println!("Rect is sufficient");
+        vec![rect]
+    } else {
+        println!("Splitting rect (total_results: {})", response.total_results.unwrap());
+        let (rect1, rect2) = split_rect(rect);
+        let mut rects = subdivide_rect(rect1).await?;
+        rects.append(&mut subdivide_rect(rect2).await?);
+        rects
+    })
+}
+
+fn split_rect(rect: geo::Rect<f64>) -> (geo::Rect<f64>, geo::Rect<f64>) {
+    if rect.width() > rect.height() {
+        let mid = rect.min().x + rect.width() / 2.;
+        (
+            geo::Rect::new(
+                geo::coord! { x: rect.min().x, y: rect.min().y },
+                geo::coord! { x: mid, y: rect.max().y },
+            ),
+            geo::Rect::new(
+                geo::coord! { x: mid, y: rect.min().y },
+                geo::coord! { x: rect.max().x, y: rect.max().y },
+            ),
+        )
+    } else {
+        let mid = rect.min().y + rect.height() / 2.;
+        (
+            geo::Rect::new(
+                geo::coord! { x: rect.min().x, y: rect.min().y },
+                geo::coord! { x: rect.max().x, y: mid },
+            ),
+            geo::Rect::new(
+                geo::coord! { x: rect.min().x, y: mid },
+                geo::coord! { x: rect.max().x, y: rect.max().y },
+            ),
+        )
+    }
+}
 
 fn observations_species_count(observations: &[inaturalist::models::Observation]) -> usize {
     observations
@@ -96,9 +169,10 @@ async fn fetch(
 
     loop {
         println!("Fetching observations");
+        INATURALIST_RATE_LIMITER.until_ready().await;
         let mut response = inaturalist::apis::observations_api::observations_get(
             &INATURALIST_REQUEST_CONFIG,
-            build_params(rect, page),
+            build_params(rect, page, 200),
         )
         .await?;
 
@@ -135,6 +209,7 @@ async fn fetch(
 fn build_params(
     rect: geo::Rect<f64>,
     page: u32,
+    per_page: u32,
 ) -> inaturalist::apis::observations_api::ObservationsGetParams {
     inaturalist::apis::observations_api::ObservationsGetParams {
         swlat: Some(rect.min().y),
@@ -144,7 +219,7 @@ fn build_params(
         // quality_grade: Some(String::from("research")),
         captive: Some(false),
         taxon_id: Some(vec![PLANTAE_ID.to_string()]),
-        per_page: Some(200.to_string()),
+        per_page: Some(per_page.to_string()),
         native: Some(true),
         page: Some(page.to_string()),
         ..Default::default()
