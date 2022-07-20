f1040ddf18dbfd1ef3a523497afa834521c40180
Corey Farwell <coreyf@rwell.org>
2022-07-19 20:15:18 -0400

    use concrete error types

--- src/geohash_observations.rs
+++ src/geohash_observations.rs
@@ -1,15 +1,49 @@
 use crate::geohash_ext::Geohash;
 use crate::Observations;
 use std::{
-    env, error,
+    env,
     io::{self, Write},
     path,
 };
 
+#[derive(thiserror::Error, Debug)]
+pub enum FetchError {
+    #[error("{0}")]
+    FetchFromCache(#[from] FetchFromCacheError),
+    #[error("{0}")]
+    FetchFromApi(#[from] FetchFromApiError),
+    #[error("{0}")]
+    WriteToCache(#[from] WriteToCacheError),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum FetchFromApiError {
+    #[error("{0}")]
+    INaturalistApi(
+        #[from] inaturalist::apis::Error<inaturalist::apis::observations_api::ObservationsGetError>,
+    ),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum FetchFromCacheError {
+    #[error("{0}")]
+    TokioIo(#[from] tokio::io::Error),
+    #[error("{0}")]
+    SerdeJson(#[from] serde_json::Error),
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum WriteToCacheError {
+    #[error("{0}")]
+    TokioIo(#[from] tokio::io::Error),
+    #[error("{0}")]
+    SerdeJson(#[from] serde_json::Error),
+}
+
 pub struct GeohashObservations(pub Geohash);
 
 impl GeohashObservations {
-    pub async fn fetch(&self) -> Result<Observations, Box<dyn error::Error>> {
+    pub async fn fetch(&self) -> Result<Observations, FetchError> {
         if let Ok(Some(observations)) = self.fetch_from_cache().await {
             return Ok(observations);
         }
@@ -19,7 +53,7 @@ impl GeohashObservations {
         Ok(observations)
     }
 
-    async fn fetch_from_cache(&self) -> Result<Option<Observations>, Box<dyn error::Error>> {
+    async fn fetch_from_cache(&self) -> Result<Option<Observations>, FetchFromCacheError> {
         let path = self.cache_path().await?;
         tracing::info!("Loading cache... ({})", path.display());
         if !path.exists() {
@@ -31,7 +65,7 @@ impl GeohashObservations {
         Ok(Some(cache))
     }
 
-    async fn fetch_from_api(&self) -> Result<Observations, Box<dyn error::Error>> {
+    async fn fetch_from_api(&self) -> Result<Observations, FetchFromApiError> {
         let subdivided_rects = crate::fetch::subdivide_rect(self.0.bounding_rect).await?;
         let num_rects = subdivided_rects.len();
         let mut observations = Vec::with_capacity(subdivided_rects.len());
@@ -42,7 +76,7 @@ impl GeohashObservations {
         Ok(observations)
     }
 
-    async fn cache_dir() -> Result<path::PathBuf, Box<dyn error::Error>> {
+    async fn cache_dir() -> tokio::io::Result<path::PathBuf> {
         let path = env::temp_dir().join("inaturalist-request-cache");
         if !path.exists() {
             tokio::fs::create_dir_all(&path).await?;
@@ -50,14 +84,11 @@ impl GeohashObservations {
         Ok(path)
     }
 
-    async fn cache_path(&self) -> Result<path::PathBuf, Box<dyn error::Error>> {
+    async fn cache_path(&self) -> tokio::io::Result<path::PathBuf> {
         Ok(Self::cache_dir().await?.join(&self.0.string))
     }
 
-    async fn write_to_cache(
-        &self,
-        observations: &Observations,
-    ) -> Result<(), Box<dyn error::Error>> {
+    async fn write_to_cache(&self, observations: &Observations) -> Result<(), WriteToCacheError> {
         let cache_path = self.cache_path().await?;
         let file = tokio::fs::File::create(cache_path).await?;
         tracing::info!("Writing cache...");
