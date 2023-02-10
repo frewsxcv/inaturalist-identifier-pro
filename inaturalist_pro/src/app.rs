3661852556114814da59932cc4c05f7a198eb46c
Corey Farwell <coreyf@rwell.org>
2023-02-10 13:50:23 -0500

    remove advertisements

--- inaturalist_pro/src/app.rs
+++ inaturalist_pro/src/app.rs
@@ -75,16 +75,6 @@ impl eframe::App for TemplateApp {
             // if ui.button("Increment").clicked() {
             //     *value += 1.0;
             // }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
