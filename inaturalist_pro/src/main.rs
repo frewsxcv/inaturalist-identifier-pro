0033c8e21c6fa755e3877839f574d31ab8fd8653
Corey Farwell <coreyf@rwell.org>
2023-02-28 17:20:32 -0500

    separate module for actor

--- inaturalist_pro/src/main.rs
+++ inaturalist_pro/src/main.rs
@@ -3,18 +3,18 @@
 
 use actix::{prelude::*, SystemRegistry};
 use geohash_ext::GeohashGrid;
-use geohash_observations::GeohashObservations;
 use image_store_actor::ImageStoreActor;
 use inaturalist::models::Observation;
 use operations::Operation;
 use std::{error, sync};
-use tokio::sync::mpsc::UnboundedSender;
+use observation_loader_actor::ObservationLoaderActor;
 
 mod app;
 mod geohash_ext;
 mod geohash_observations;
 mod image_store;
 mod image_store_actor;
+mod observation_loader_actor;
 mod operations;
 mod places;
 mod taxon_tree;
@@ -37,52 +37,6 @@ lazy_static::lazy_static! {
 }
 
 type CurOperation = operations::TopImageScore;
-
-struct ObservationLoaderActor {
-    tx_app_message: UnboundedSender<AppMessage>,
-    grid: GeohashGrid,
-    tx_load_observations: UnboundedSender<Observation>,
-}
-
-/// Declare actor and its context
-impl Actor for ObservationLoaderActor {
-    type Context = Context<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-        let grid = self.grid.clone();
-        let tx_load_observations = self.tx_load_observations.clone();
-        let tx_app_message = self.tx_app_message.clone();
-        let t = async move {
-            for (i, geohash) in grid.clone().0.into_iter().enumerate() {
-                tracing::info!(
-                    "Fetch observations for geohash {} ({} / {})",
-                    geohash.string,
-                    i + 1,
-                    grid.0.len(),
-                );
-                GeohashObservations(geohash)
-                    .fetch_from_api(
-                        tx_load_observations.clone(),
-                        &FETCH_SOFT_LIMIT,
-                        CurOperation::request(),
-                    )
-                    .await
-                    .unwrap();
-                tx_app_message.clone().send(AppMessage::Progress).unwrap();
-            }
-            // FIXME: call below
-            // operation.lock().await.finish();
-            tracing::info!("Finished loading thread");
-            // tx.send(AppMessage::Results(std::mem::take(
-            //     &mut operation.0,
-            // )))
-            // .await
-            // .unwrap();
-        };
-
-        ctx.spawn(Box::pin(t).into_actor(self));
-    }
-}
 
 #[actix::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
