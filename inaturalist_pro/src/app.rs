1bea6c875f62a442a4c2770207bcc5f49688d34d
Corey Farwell <coreyf@rwell.org>
2023-03-24 01:02:24 -0400

    more condensed styling

--- inaturalist_pro/src/app.rs
+++ inaturalist_pro/src/app.rs
@@ -5,7 +5,7 @@ use std::sync;
 
 use crate::{
     image_store_actor::{ImageStoreActor, LoadImageMessage},
-    taxa_store::{TaxaValue, TaxaStore},
+    taxa_store::{TaxaStore, TaxaValue},
     taxon_tree_builder_actor::{BuildTaxonTreeMessage, TaxonTreeBuilderActor},
 };
 
@@ -75,7 +75,9 @@ impl eframe::App for App {
                     self.loaded_geohashes += 1;
                 }
                 crate::AppMessage::TaxonLoaded(taxon) => {
-                    self.taxa_store.0.insert(taxon.id.unwrap(), (&*taxon).into());
+                    self.taxa_store
+                        .0
+                        .insert(taxon.id.unwrap(), (&*taxon).into());
                 }
                 crate::AppMessage::Result((observation, scores)) => {
                     self.handle_new_result(observation, scores)
@@ -172,38 +174,51 @@ impl<'a> egui::Widget for TaxonTreeWidget<'a> {
             self.root_node.taxon_id,
         );
 
-        let title = match self.taxa_store.0.get(&self.root_node.taxon_id) {
-            Some(TaxaValue::Loaded(taxon)) => {
-                format!("{} ({})", self.root_node.taxon_id, taxon.name,)
-            }
-            Some(TaxaValue::Loading) => "Loading...".to_string(),
-            None => "<Unknown>".to_string(),
+        let color = if self.root_node.score > 75. {
+            Color32::GREEN
+        } else if self.root_node.score > 50. {
+            Color32::YELLOW
+        } else if self.root_node.score > 25. {
+            // Orange
+            Color32::from_rgb(255, 165, 0)
+        } else if self.root_node.score > 0. {
+            Color32::RED
+        } else {
+            Color32::GRAY
         };
 
-        egui::CollapsingHeader::new(title)
-            .id_source(collapsing_header_id)
-            .default_open(true)
-            .show(ui, |ui| {
-                let color = if self.root_node.score > 75. {
-                    Color32::BLUE
-                } else if self.root_node.score > 50. {
-                    Color32::GREEN
-                } else if self.root_node.score > 25. {
-                    Color32::YELLOW
-                } else if self.root_node.score > 0. {
-                    Color32::RED
-                } else {
-                    Color32::GRAY
-                };
-                ui.colored_label(color, format!("Score: {}", self.root_node.score));
-                for node in self.root_node.children.0.iter() {
-                    ui.add(TaxonTreeWidget {
-                        observation: self.observation,
-                        root_node: node,
-                        taxa_store: self.taxa_store,
-                    });
+        let (response, _, _) = egui::collapsing_header::CollapsingState::load_with_default_open(
+            ui.ctx(),
+            collapsing_header_id.into(),
+            true,
+        )
+        .show_header(ui, |ui| {
+            match self.taxa_store.0.get(&self.root_node.taxon_id) {
+                Some(TaxaValue::Loaded(taxon)) => {
+                    if ui.button("✔").clicked() {}
+                    ui.colored_label(
+                        color,
+                        format!("{} ({})", taxon.name, self.root_node.score.round()),
+                    );
                 }
-            })
-            .header_response
+                Some(TaxaValue::Loading) => {
+                    unimplemented!()
+                }
+                None => {
+                    ui.spinner();
+                    ui.label("Loading");
+                }
+            }
+        })
+        .body(|ui| {
+            for node in self.root_node.children.0.iter() {
+                ui.add(TaxonTreeWidget {
+                    observation: self.observation,
+                    root_node: node,
+                    taxa_store: self.taxa_store,
+                });
+            }
+        });
+        response
     }
 }
